9de2427017770ba8236bfdc50ce80e56aa3a0acc
Southern Wolf <southernwolf1995@gmail.com>
2022-01-09 22:19:54 -0500

    Improved Barcode SVG Rendering
     - Changed barcode rendering to be dynamically generated using JavaScript
    
    - Added initial changes in preparation for adding more rendering styles
    
    - Cleaned up JavaScript and HTML code

--- main.js
+++ main.js
@@ -13,15 +13,14 @@ setInterval(setTimer, 1000)
 function startProviderListener() {
     // Get Ether Block Hash Data
     provider.on("block", (blockNumber) => {
-        //console.log(blockNumber)
         getBlockData(blockNumber)
         largest_block = blockNumber
     })
 }
 async function getBlockData(blockNumber) {
     let blockData = await provider.getBlock(blockNumber)
+
     blockData = JSON.stringify(blockData.hash)
-    //console.log(blockData.substr(3, blockData.length-4))
 
     createBarFlag(blockData.substr(3, blockData.length - 4), blockNumber)
     secondsSinceLastBlock = 0
@@ -30,9 +29,9 @@ async function getBlockData(blockNumber) {
 // Show Static Block Data From User Input
 function getUserBlockNumber() {
     let blockNumber = 0
+
     if (document.getElementById("blockNum").value != "" && document.getElementById("blockNum").value < largest_block) {
         blockNumber = parseInt(document.getElementById("blockNum").value)
-        //console.log(blockNumber)
         getStaticBlockData(blockNumber)
     } else {
         console.log("Invalid input: NaN or block size is too large.")
@@ -40,37 +39,46 @@ function getUserBlockNumber() {
 }
 async function getStaticBlockData(blockNumber) {
     provider.off("block")
-    
-    //console.log(blockNumber)
+
     let blockData = await provider.getBlock(blockNumber)
     blockData = JSON.stringify(blockData.hash)
-    //console.log(blockData.substr(3, blockData.length-4))
 
     createBarFlag(blockData.substr(3, blockData.length - 4), blockNumber)
 }
 
 // Create Barcode Pattern From Block Hash
 function createBarFlag(blockHash, blockNumber) {
-    let transaction_id = blockHash
+    const CHAR_IDENTIFIER = blockHash.slice(60).toUpperCase()
+    const BLOCKHASHARRAY = blockHash.split('')
+    const WIDTH = 100, HEIGHT = 1000
+    let blockHashArrayCounter = 1
+    let colorHex = ''
+    let colorHexArray = []
+    let colorHexArrayCounter = 0
+    let svgRect = ''
 
-    let background_color = [transaction_id.slice(0, 6), transaction_id.slice(6, 12), transaction_id.slice(12, 18), transaction_id.slice(18, 24), transaction_id.slice(24, 30), transaction_id.slice(30, 36), transaction_id.slice(36, 42), transaction_id.slice(42, 48), transaction_id.slice(48, 54), transaction_id.slice(54, 60)]
-    let char_identifier = transaction_id.slice(60).toUpperCase()
-    document.getElementById("one").style = "fill: #" + background_color[0] + ""
-    document.getElementById("two").style = "fill: #" + background_color[1] + ""
-    document.getElementById("three").style = "fill: #" + background_color[2] + ""
-    document.getElementById("four").style = "fill: #" + background_color[3] + ""
-    document.getElementById("five").style = "fill: #" + background_color[4] + ""
-    document.getElementById("six").style = "fill: #" + background_color[5] + ""
-    document.getElementById("seven").style = "fill: #" + background_color[6] + ""
-    document.getElementById("eight").style = "fill: #" + background_color[7] + ""
-    document.getElementById("nine").style = "fill: #" + background_color[8] + ""
-    document.getElementById("ten").style = "fill: #" + background_color[9] + ""
-    document.getElementById("char").innerHTML = char_identifier
+    for (let index = 0; index < BLOCKHASHARRAY.length; index++) {
+        colorHex = colorHex + BLOCKHASHARRAY[index]
+        if (blockHashArrayCounter == 6) {
+            colorHexArray[colorHexArrayCounter] = colorHex
+            blockHashArrayCounter = 0
+            colorHex = ''
+            colorHexArrayCounter++
+        }
+        blockHashArrayCounter++
+    }
+
+    document.getElementById("barcode").innerHTML = ''
     
+    for (let index = 0; index < colorHexArray.length; index++) {
+        svgRect = svgRect + `<rect id="" width="${WIDTH}" height="${HEIGHT}" x="${index*100}" style="fill: #${colorHexArray[index]}" />`
+    }
+    svgRect = svgRect + `<text id="char" x="2" y="950" fill="white"
+    style="position: absolute; color: white; font-size: 165px; font-family: 'Roboto Mono'; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">${CHAR_IDENTIFIER}</text>`
+
+    document.getElementById("barcode").innerHTML = svgRect
     document.getElementById("userName").innerHTML = "Ethereum Block: " + blockNumber
-
     document.getElementById("label").innerHTML = "Enter Block Number (Max of " + largest_block + ")"
-
 }
 
 function setTimer() {
