bd99fb9f48e561182e94e44e0b38f7bb046c52cc
Southern Wolf <southernwolf1995@gmail.com>
2022-01-10 03:39:10 -0500

    Improved SVG Rendering
    - Provider no longer needs to be disabled to show static block data
    
    - Max block size and time since last block will now be accurate when viewing static block data

--- main.js
+++ main.js
@@ -4,30 +4,45 @@
 const NETWORK = "homestead";
 const provider = ethers.getDefaultProvider(NETWORK, {});
 let largest_block
+let showStaticData = false
 
 // Set Timer Defaults
 let secondsSinceLastBlock = 0
 setInterval(setTimer, 1000)
 
+function setTimer() {
+    document.getElementById("sec").innerHTML = secondsSinceLastBlock
+
+    secondsSinceLastBlock++
+}
+
 // Show Latest Block Data
-function startProviderListener() {
+async function startProviderListener() {
     // Get Ether Block Hash Data
     provider.on("block", (blockNumber) => {
         getBlockData(blockNumber)
         largest_block = blockNumber
+        secondsSinceLastBlock = 0
+        document.getElementById("label").innerHTML = "Enter Block Number (Max of " + largest_block + ")"
     })
 }
 async function getBlockData(blockNumber) {
-    let blockData = await provider.getBlock(blockNumber)
+    if (!showStaticData) {
+        let blockData = await provider.getBlock(blockNumber)
+        blockData = JSON.stringify(blockData.hash)
+        createBarFlag(blockData.substr(3, blockData.length - 4), blockNumber)
+    }
+}
 
-    blockData = JSON.stringify(blockData.hash)
-
-    createBarFlag(blockData.substr(3, blockData.length - 4), blockNumber)
-    secondsSinceLastBlock = 0
+function showLiveData() {
+    showStaticData = false
+    getBlockData(largest_block)
 }
 
 // Show Static Block Data From User Input
 function getUserBlockNumber() {
+    showStaticData = true
+
     let blockNumber = 0
 
     if (document.getElementById("blockNum").value != "" && document.getElementById("blockNum").value < largest_block) {
@@ -38,8 +53,6 @@ function getUserBlockNumber() {
     }
 }
 async function getStaticBlockData(blockNumber) {
-    provider.off("block")
-
     let blockData = await provider.getBlock(blockNumber)
     blockData = JSON.stringify(blockData.hash)
 
@@ -50,7 +63,8 @@ async function getStaticBlockData(blockNumber) {
 function createBarFlag(blockHash, blockNumber) {
     const CHAR_IDENTIFIER = blockHash.slice(60).toUpperCase()
     const BLOCKHASHARRAY = blockHash.split('')
-    const WIDTH = 100, HEIGHT = 1000
+    const WIDTH = 100,
+          HEIGHT = 1000
     let blockHashArrayCounter = 1
     let colorHex = ''
     let colorHexArray = []
@@ -69,20 +83,12 @@ function createBarFlag(blockHash, blockNumber) {
     }
 
     document.getElementById("barcode").innerHTML = ''
-    
+
     for (let index = 0; index < colorHexArray.length; index++) {
-        svgRect = svgRect + `<rect id="" width="${WIDTH}" height="${HEIGHT}" x="${index*100}" style="fill: #${colorHexArray[index]}" />`
+        svgRect = svgRect + `<rect width="${WIDTH}" height="${HEIGHT}" x="${index*100}" style="fill: #${colorHexArray[index]}" />`
     }
-    svgRect = svgRect + `<text id="char" x="2" y="950" fill="white"
-    style="position: absolute; color: white; font-size: 165px; font-family: 'Roboto Mono'; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">${CHAR_IDENTIFIER}</text>`
+    svgRect = svgRect + `<text id="barcode-char" x="2" y="950" fill="white">${CHAR_IDENTIFIER}</text>`
 
     document.getElementById("barcode").innerHTML = svgRect
     document.getElementById("userName").innerHTML = "Ethereum Block: " + blockNumber
-    document.getElementById("label").innerHTML = "Enter Block Number (Max of " + largest_block + ")"
-}
-
-function setTimer() {
-    document.getElementById("sec").innerHTML = secondsSinceLastBlock
-
-    secondsSinceLastBlock++
 }
